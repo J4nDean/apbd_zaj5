cd52e64826a7f9fd2e30d90d26fcfd8edd2efa37
s24755 <s24755@pjwstk.edu.pl>
2024-04-08 11:38:50 +0200

    commit some stuff

--- WebApplication1/WebApplication1/controller/AnimalsController.cs
+++ WebApplication1/WebApplication1/controller/AnimalsController.cs
@@ -15,8 +15,8 @@ public class AnimalsController : ControllerBase
         return _animals;
     }
 
-    [HttpGet]
-    public ActionResult<Animal> GetAniaml(int id)
+    [HttpGet("{id}")]
+    public ActionResult<Animal> GetAnimal(int id)
     {
         var animal = _animals.FirstOrDefault(a => a.Id == id);
         if (animal == null)
@@ -26,4 +26,29 @@ public class AnimalsController : ControllerBase
 
         return animal;
     }
+
+    [HttpPost]
+    public ActionResult<Animal> CreatAniaml(Animal animal)
+    {
+        _animals.Add(animal);
+        return CreatedAtAction(nameof(GetAnimal), new { id = animal.Id }, animal);
+    }
+
+    [HttpPut("{id}")]
+    public IActionResult UpdateAniaml(int id, Animal animal)
+    {
+        var existingAnimal = _animals.FirstOrDefault(a => a.Id == id);
+        if (existingAnimal == null)
+        {
+            return NotFound();
+        }
+
+        existingAnimal.Name = animal.Name;
+        existingAnimal.Typ = animal.Typ;
+        existingAnimal.Weight = animal.Weight;
+        existingAnimal.FurColor = animal.FurColor;
+
+        return NoContent();
+    }
 }
+
---
+++ WebApplication1/WebApplication1/interface/IAnimalService.cs
@@ -0,0 +1,10 @@
+﻿using WebApplication1.models;
+
+public interface IAnimalService
+{
+    IEnumerable<Animal> GetAnimals();
+    Animal GetAnimal(int id);
+    Animal CreateAnimal(Animal animal);
+    Animal UpdateAnimal(int id, Animal animal);
+    void DeleteAnimal(int id);
+}

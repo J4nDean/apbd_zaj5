3a7ddebd6ea5e857cf1e5a2acd96a5ad798b0f3b
s24755 <s24755@pjwstk.edu.pl>
2024-04-08 11:11:49 +0200

    commit add controller and models

---
+++ WebApplication1/WebApplication1/controller/AnimalsController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication1.models;
+
+namespace WebApplication1.controller;
+
+[ApiController]
+[Route("[controller]")]
+public class AnimalsController : ControllerBase
+{
+    private readonly List<Animal> _animals = new List<Animal>();
+
+    [HttpGet]
+    public IEnumerable<Animal> GetAnimals()
+    {
+        return _animals;
+    }
+
+    [HttpGet]
+    public ActionResult<Animal> GetAniaml(int id)
+    {
+        var animal = _animals.FirstOrDefault(a => a.Id == id);
+        if (animal == null)
+        {
+            return NotFound();
+        }
+
+        return animal;
+    }
+}
---
+++ WebApplication1/WebApplication1/models/Animal.cs
@@ -0,0 +1,10 @@
+﻿namespace WebApplication1.models;
+
+public class Animal
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Type { get; set; }
+    public double masa { get; set; }
+    public string kolorSiersci { get; set; }
+}
